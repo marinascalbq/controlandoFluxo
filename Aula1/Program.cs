bd63dd8052f1fd31f2a1a029bbacb9207820c764
Marina <marinascalbuquerque@gmail.com>
2022-03-12 15:49:19 -0300

    first commit

---
+++ Aula1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Aula1
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int valor1;
+            int valor2; 
+            int total;
+
+            //Primeiro Fluxo
+            Console.WriteLine("Digite o primeiro valor: ");
+            valor1 = int.Parse (Console.ReadLine());
+            Console.WriteLine("O primeiro valor é " + valor1);
+
+            //Segundo Fluxo
+            Console.WriteLine("Digite o segundo valor: ");
+            valor2 = int.Parse (Console.ReadLine());
+            Console.WriteLine("O segundo valor é " + valor2);
+
+            //Terceiro Fluxo
+            total = valor1 + valor2;
+            Console.WriteLine("A soma dos dois valores é " + total);
+        }
+    }
+}
+
---
+++ Aula4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Aula3
+{
+    class Program
+    {
+        static void Main ( string[] args)
+        {
+   
+        Boolean condicao = true;
+            int valor;
+
+            do {
+            Console.WriteLine ("Digite um valor, 0 para sair");
+            valor = int.Parse(Console.ReadLine ());
+
+            //"break" e "continue"
+            if (valor == 0)
+                {
+                 Console.WriteLine("Você saiu da aplicação");
+                break;
+                }
+        else 
+        {
+            Console.WriteLine ("O valor informado é " + valor );
+            continue ;
+            }   
+        } while (condicao== true);
+            
+        }
+    }
+}
